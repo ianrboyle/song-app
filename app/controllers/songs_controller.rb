35d9154c9c870f4e3ebc49362daf1ad4ee1557cc
Ian Boyle <irboyle21@gmail.com>
2021-10-18 12:31:13 -0400

    adds index and show method

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,11 @@
 class SongsController < ApplicationController
   def index
     songs = Song.all
+    render json: {message: songs}
+  end
+  def show
+    song_id = params[:id]
+    song = Song.find_by(id: song_id)
     render json: {message: song}
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ApplicationRecord
+  # 5.times do
+  #   Song.create(title: FFaker::Music.song, album: FFaker::Music.album, artist: FFaker::Music.artist, year: FFaker::Vehicle.year)
+  # end
+
 end
