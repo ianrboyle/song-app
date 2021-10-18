662b323a1b99741d9790111c046706cfec425773
Ian Boyle <irboyle21@gmail.com>
2021-10-18 12:51:38 -0400

    adds delete route/method

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,7 +10,22 @@ class SongsController < ApplicationController
   end
   def create
     song = Song.create(title: params["title"], artist: params["artist"], year: params["year"], album: params["album"])
-
     render json: song
+  end
+  def update
+    song_id = params[:id]
+    song = Song.find_by(id: song_id)
+    song.title = params["title"] || song.title
+    song.artist = params["artist"] || song.artist
+    song.year = params["year"] || song.year
+    song.album = params["album"] || song.album
+    song.save
+    render json: song
+  end
+  def destroy
+    song_id = params[:id]
+    song = Song.find_by(id: song_id)
+    song.delete
+    render json: {message: "Song Destroyed"}
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
-  # 5.times do
-  #   Song.create(title: FFaker::Music.song, album: FFaker::Music.album, artist: FFaker::Music.artist, year: FFaker::Vehicle.year)
-  # end
+  5.times do
+    Song.create(title: FFaker::Music.song, album: FFaker::Music.album, artist: FFaker::Music.artist, year: FFaker::Vehicle.year)
+  end
 
 end
